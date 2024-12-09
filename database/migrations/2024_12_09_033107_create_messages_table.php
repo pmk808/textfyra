f463fc3f0d4f72e59b63496ae7b4c5140c2c085c
Peemak <cabanesmcphy123@gmail.com>
2024-12-09 12:27:24 +0800

    feature(auth): installed laravel breeze

---
+++ database/migrations/2024_12_09_033107_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('recipient_type'); // 'individual' or 'group'
+            $table->string('recipient_id'); // student_id or program name
+            $table->text('message_content');
+            $table->string('status'); // sent, failed, pending
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
